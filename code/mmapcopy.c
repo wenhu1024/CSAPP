cd06ed3868ea513e7b2b1e058313fde05515248f
wenhu1024 <1340773297@qq.com>
2024-03-25 15:30:43 +0800

    modify size

--- code/mmapcopy.c
+++ code/mmapcopy.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 int main(int argc,char *argv[]){
     if(argc!=2){
@@ -10,15 +11,17 @@ int main(int argc,char *argv[]){
     }
 
     int fd=open(argv[1],O_RDWR);
+    struct stat stat;
+    fstat(fd,&stat);
 
     char *p;
-    p=mmap(NULL,5,PROT_READ,MAP_SHARED,fd,0);
+    p=mmap(NULL,stat.st_size,PROT_READ,MAP_SHARED,fd,0);
     if(p==MAP_FAILED){
         perror("mmap error");
         exit(1);
     }
 
-    write(STDOUT_FILENO,p,5);
+    write(STDOUT_FILENO,p,stat.st_size);
 
 
     return 0;
